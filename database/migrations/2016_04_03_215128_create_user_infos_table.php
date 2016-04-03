e0d6e34ce49498887be5c60923aaed6557d2dc2e
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-03 18:53:08 -0300

    new model

---
+++ database/migrations/2016_04_03_215128_create_user_infos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUserInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('phone_number');
+            $table->string('address')->nullable();
+            $table->integer('state_id')->unsigned();
+            $table->foreign('state_id')->references('id')->on('states')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_infos');
+    }
+}
