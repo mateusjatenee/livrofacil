6bf4f1f57afaaf8882e1cd0171657358eb91cec8
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-03 21:14:19 -0300

    Ad model

---
+++ database/migrations/2016_04_04_000813_create_ads_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->date('date');
+            $table->bool('is_donation');
+            $table->bool('accepts_trades')->nullable();
+            $table->integer('price')->nullable();
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ads');
+    }
+}
