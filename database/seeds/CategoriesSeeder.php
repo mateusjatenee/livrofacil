60e3de27e4ecec402151d99c98f5763c1c530340
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-03 23:17:19 -0300

    wth

---
+++ database/seeds/CategoriesSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::insert([
+            ['name' => 'Comics'],
+            ['name' => 'Revistas'],
+        ]);
+    }
+}
---
+++ tests/app/Http/Controllers/Api/AdsControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\App\Http\Controllers\Api;
+
+use App\Ad;
+use App\Category;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use TestCase;
+
+class AdsControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+    use WithoutMiddleware;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testIndexStatusCodeShouldBe200()
+    {
+
+        $this->visit('/api/ads')->seeStatusCode(200);
+    }
+
+    public function testIndexShouldHaveAllRecords()
+    {
+        $category = factory(Category::class)->create();
+
+        $user = factory(User::class)->create();
+
+        $ad = factory(Ad::class)->make(['category_id' => $category->id]);
+
+        $user->ads()->save($ad);
+
+        $this->get('api/ads')->seeJson(['title' => 'Animal Farm'])->assertResponseStatus(200);
+
+    }
+}
